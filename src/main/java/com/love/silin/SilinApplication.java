bf5e57e8635610de5cf0e266275a3dc0bc7f148a
wangsiming <swang2_intern@xiaohongshu.com>
2018-09-27 11:52:56 +0800

    init spring-lovespace

---
+++ src/main/java/com/love/silin/SilinApplication.java
@@ -0,0 +1,12 @@
+package com.love.silin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilinApplication.class, args);
+	}
+}
---
+++ src/test/java/com/love/silin/SilinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.love.silin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SilinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
